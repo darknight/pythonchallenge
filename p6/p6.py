9252a3b59710023d627d8b652b4a1f7506299e33
ffyao <illuminating.me@gmail.com>
2013-08-10 11:25:41 +0800

    finished problem 6, well, googled for some hints

--- p6/p6.py
+++ p6/p6.py
@@ -1,17 +1,37 @@
 import re
+import os
+import sys
+import zipfile
+import shutil
 
 next_hint = re.compile(r'[A-Za-z\s]+(\d+)')
 next_file = 90052
 
-def find_next():
+def main(zipobj, dirname):
+    global next_file
+    comments = []
     while True:
         fname = str(next_file) + '.txt'
-        with open(fname) as f:
+        path = dirname + '/' + fname
+        with open(path) as f:
+            cmt = zipobj.getinfo(fname).comment
+            comments.append(cmt)
             line = f.readline()
             res = next_hint.search(line)
             if res:
                 next_file = res.group(1)
             else:
-                print fname
-                print line
                 break
+    print ''.join(comments)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print 'provide path of channel.zip'
+        sys.exit(1)
+    fname = sys.argv[1]
+    zipobj = zipfile.ZipFile(fname)
+    todir = os.getcwd() + '/tmp'
+    zipobj.extractall(path=todir)
+    main(zipobj, todir)
+    shutil.rmtree(todir)
