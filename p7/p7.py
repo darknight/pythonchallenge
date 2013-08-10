db5b05a4b8f6e1eb84c5172ab6d02a55bbdfac90
ffyao <illuminating.me@gmail.com>
2013-08-10 20:20:20 +0800

    p7 is solved, but not elegant, see this link: http://garethrees.org/2007/05/07/python-challenge/

---
+++ p7/p7.py
@@ -0,0 +1,20 @@
+import Image
+
+def main():
+    im = Image.open('oxygen.png')
+    width, height = im.size
+    box = (0, height / 2, width, height / 2 + 1)
+    region = im.crop(box)
+    data = region.getdata()
+    word = []
+    for d in data:
+        r, g, b, a = d
+        word.append(chr(r))
+    print 'word is'
+    print ''.join(word)
+
+if __name__ == '__main__':
+    main()
+    # get hint from main(), not very elegant
+    hint = (105, 110, 116, 101, 103, 114, 105, 116, 121,)
+    print ''.join(map(chr, hint))
